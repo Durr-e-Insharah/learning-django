2168cfe61b05a2966f1e8d85a3a454e1fed2b911
Insharah <insharah41@gmail.com>
2026-09-01 16:51:54 +0500

    Added Review, Bookstore, Award models with relationships and admin customization

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,25 @@
 from django.contrib import admin
 # Register your models here.
-from .models import Book
-admin.site.register(Book)
+from django.contrib import admin
+from .models import Book, Review, Bookstore, Award
+
+class ReviewInline(admin.TabularInline):
+    model = Review
+    extra = 2
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'price')
+    list_display_links = ('title',)
+    list_filter = ('author',)
+    inlines = [ReviewInline]
+
+class BookstoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+    filter_horizontal = ('books',)
+
+class AwardAdmin(admin.ModelAdmin):
+    list_display = ('award_name', 'book', 'year_won')
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Bookstore, BookstoreAdmin)
+admin.site.register(Award, AwardAdmin)
--- core/models.py
+++ core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
@@ -10,3 +11,31 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reviews')
+    reviewer_name = models.CharField(max_length=100)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.reviewer_name} - {self.book.title}"
+
+
+class Bookstore(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book, related_name='stores')
+
+    def __str__(self):
+        return self.name
+
+class Award(models.Model):
+    book = models.OneToOneField(Book, on_delete=models.CASCADE, related_name='award')
+    award_name = models.CharField(max_length=100)
+    year_won = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.award_name} for {self.book.title}"
